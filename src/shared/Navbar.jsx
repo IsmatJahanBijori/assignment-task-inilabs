4100151a02e6d2b9940d522d62364a894c2963a5
Ismat Jahan Bijori <ismatjahanbijori@google.com>
2024-03-02 00:33:17 +0600

    hero section added

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-// <a href="https://ibb.co/2MjHqq4"><img src="https://i.ibb.co/94qSnn5/svg-1.png" alt="svg-1" border="0"></a>
-// <a href="https://ibb.co/x7V8fGX"><img src="https://i.ibb.co/DR3g8kz/div-after.png" alt="div-after" border="0"></a>
+
 const Navbar = () => {
     return (
         <>
-            <header className="">
-                <div className=" mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <header className="h-[90px] px-5 text-base inter-text">
+                <div className="mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                     <NavLink className="flex font-medium items-center  mb-4 md:mb-0">
                         <img src="https://i.ibb.co/7y8F1Gq/svg.png" alt="Miro" />
                     </NavLink>
-                    <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+                    <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 flex flex-wrap items-center text-base justify-center">
                         <NavLink className="mr-2 ">Product</NavLink>
                         <img src="https://i.ibb.co/DR3g8kz/div-after.png" alt="" className='mr-7' />
                         <NavLink className="mr-2 ">Solutions</NavLink>
