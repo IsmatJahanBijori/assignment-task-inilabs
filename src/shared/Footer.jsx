047de2784d4ae2aaea7a21b45b4f1d612679da6c
Ismat Jahan Bijori <ismatjahanbijori@google.com>
2024-03-03 20:25:36 +0600

    footer added

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import Container from '../components/Container'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className='w-full bg-[#02033B] mt-24'>
+            {/* <Container className=" w-[1056px] bg-gray-400 "> */}
+            <div className='flex flex-col md:flex-row px-28 py-14 justify-evenly'>
+                {/* 1 */}
+                <div className='flex flex-col w-[312px]'>
+                    {/* left */}
+                    <div className='bg-red-300'>
+                        <p className='footer-typo'>Scan. Detect. Remove.</p>
+                        <div className='grid grid-cols-3 gap-10 mt-[78px]'>
+                            <img src="https://i.ibb.co/gZdwc7n/svg.png" alt="" />
+                            <img src="https://i.ibb.co/b2JqkX5/svg-1.png" alt="" />
+                            <img src="https://i.ibb.co/Pz567px/svg-2.png" alt="" />
+                        </div>
+                        <div className='mt-6 link '>
+                            <li><a href="">Privacy Policy</a></li>
+                            <li><a href="">Terms of Service</a></li>
+                        </div>
+                    </div>
+                    <div className='bg-green-400 mt-12 link-p'>
+                        <p>Copyright © 2022 Certo Software Limited | Registered in England & Wales No. 10072356</p>
+                        <p className='mt-3'>Designed & developed by Bigger Picture</p>
+
+                    </div>
+                </div>
+                {/* middle */}
+                <div className='bg-purple-400 w-[188px] gap-15'>
+                    <p className='miro'>Miro.</p>
+                    <div className='miro-p'>
+                        <p className='flex place-items-center mt-5'><span><hr className='text-[#FFC247] mr-2 border w-3.5' /></span> iPhone</p>
+                        <p className='flex place-items-center mt-5'><span><hr className='text-[#FFC247] mr-2 border w-3.5' /></span>Android</p>
+                        <p className='flex place-items-center mt-5'><span><hr className='text-[#FFC247] mr-2 border w-3.5' /></span>Help</p>
+                        <p className='flex place-items-center mt-5'><span><hr className='text-[#FFC247] mr-2 border w-3.5' /></span>About</p>
+                        <p className='flex place-items-center mt-5'><span><hr className='text-[#FFC247] mr-2 border w-3.5' /></span>Insights</p>
+                    </div>
+                </div>
+                {/* right */}
+                <div className='bg-[#FFC247] w-[436px] h-[270px] signup'>
+                    <p className="signup-p">Sign up to our newsletter</p>
+                    <p className='form-p'>Receive the latest mobile security news, exclusive discounts & offers straight to your inbox!</p>
+
+                    <div class="relative flex h-10 mt-10 w-full flex-row overflow-clip rounded-lg">
+                        <input class="peer w-full rounded-l-full border px-2  text-[#02033B80] transition-colors duration-300 pl-[10px] focus:outline-none" type="text" name="domain" id="domain" placeholder="Email address" />
+                        <label class="flex items-center rounded-r-full border text-white px-2 text-sm  bg-[#02033B] transition-colors duration-300 object-cover w-[100px]" for="domain">Submit</label>
+                    </div>
+
+
+                </div>
+            </div>
+            <div className='w-[550px] flex flex-col md:flex-row ml-10 px-28 py-14'>
+                <p className='link-p'>Apple, the Apple logo, and iPhone are trademarks of Apple Inc., registered in the U.S. and other countries. App Store is a service mark of Apple Inc. Android, Google Play and the Google Play logo are trademarks of Google LLC.</p>
+            </div>
+            {/* </Container> */}
+        </div>
     )
 }
 
