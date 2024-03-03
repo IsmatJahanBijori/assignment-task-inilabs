4ae7a655908db9b1129bf1fdd37024c13cd9c71b
Ismat Jahan Bijori <ismatjahanbijori@google.com>
2024-03-03 22:28:30 +0600

    navabr responsive

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,57 +1,101 @@
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import { GiHamburgerMenu } from "react-icons/gi";
-import { RxCross2 } from "react-icons/rx";
-import Container from '../components/Container';
+
+
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { NavLink } from 'react-router-dom';
 import { MdKeyboardArrowDown } from "react-icons/md";
 import Button from '../components/Button';
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
+    // State to manage the navbar's visibility
+    const [nav, setNav] = useState(false);
+
+    // Toggle function to handle the navbar's display
+    const handleNav = () => {
+        setNav(!nav);
     };
+
     const navLists = (
         <>
-            <NavLink className="flex font-medium items-center  mb-4 md:mb-0">
-                <img src="https://i.ibb.co/7y8F1Gq/svg.png" alt="Miro" />
-            </NavLink>
-            <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 flex flex-wrap items-center text-base justify-center">
-                <NavLink className="mr-2 ">Product</NavLink>
-                <MdKeyboardArrowDown className='mr-7 size-5' />
-                <NavLink className="mr-2 ">Solutions</NavLink>
-                <MdKeyboardArrowDown className='mr-7 size-5' />
-                <NavLink className="mr-2 ">Resources</NavLink>
-                <MdKeyboardArrowDown className='mr-7 size-5' />
-                <NavLink className="mr-7 ">Enterprise</NavLink>
-                <NavLink className="mr-16 ">Pricing</NavLink>
+
+            <nav className="lg:mr-auto lg:ml-4 lg:py-1 lg:pl-4 flex flex-col lg:flex-row  items-center text-base justify-start lg:justify-center">
+                <div className='flex lg:flex-row mb-2 lg:mb-0'>
+                    <li><NavLink className="lg:mr-2 ">Product</NavLink></li>
+                    <MdKeyboardArrowDown className='lg:mr-7 size-5' />
+                </div>
+                <div className='flex lg:flex-row mb-2 lg:mb-0'>
+                    <li><NavLink className="lg:mr-2 ">Solutions</NavLink></li>
+                    <MdKeyboardArrowDown className='lg:mr-7 size-5' />
+                </div>
+                <div className='flex lg:flex-row mb-3 lg:mb-0'>
+                    <li><NavLink className="lg:mr-2 ">Resources</NavLink></li>
+                    <MdKeyboardArrowDown className='lg:mr-7 size-5' />
+                </div>
+                <li className='mb-2 lg:mb-0'><NavLink className="lg:mr-7 ">Enterprise</NavLink></li>
+                <li className='mb-2 lg:mb-0'><NavLink className="lg:mr-16">Pricing</NavLink></li>
             </nav>
-            <div className='flex place-items-center'>
-                <img src="https://i.ibb.co/94qSnn5/svg-1.png" alt="" className='size-6 mr-2' />
-                <NavLink className="mr-7">EN</NavLink>
-                <NavLink className="mr-7 ">Contact Sales</NavLink>
-                <NavLink className="mr-10 ">Login</NavLink>
+            <div className='flex place-items-center flex-col lg:flex-row text-base'>
+                <div className='flex lg:flex-row gap-3 lg:gap-0 mb-3 lg:mb-0'>
+                    <img src="https://i.ibb.co/94qSnn5/svg-1.png" alt="" className='size-6 lg:mr-2' />
+                    <li><NavLink className="lg:mr-7">EN</NavLink></li>
+                </div>
+                <li className='mb-2 lg:mb-0'><NavLink className="lg:mr-7 ">Contact Sales</NavLink></li>
+                <li className='mb-2 lg:mb-0'><NavLink className="lg:mr-10 ">Login</NavLink></li>
                 <Button />
             </div>
         </>
     )
-    return (
-        // bg-[#F0F0F0]
 
-        <>
-            <header className="h-[90px] px-5 bg-[#F0F0F0] text-base inter-text overflow-y-hidden">
-                <div className="mx-auto flex p-5 flex-row items-center">
-                    <div className="flex items-center md:hidden">
-                        <button onClick={toggleMenu}>
-                            <GiHamburgerMenu className="text-2xl" />
-                        </button>
-                    </div>
+    return (
+        <div className=' px-5 bg-[#F0F0F0] flex justify-between text-base inter-text overflow-y-hidden'>
+            {/* Logo */}
+
+            <NavLink className="flex font-medium items-center my-4 lg:my-0">
+                <img src="https://i.ibb.co/7y8F1Gq/svg.png" alt="Miro" />
+            </NavLink>
+            {/* Desktop Navigation */}
+            <ul className='hidden lg:flex'>
+
+                <li
+
+                    className='mx-auto flex p-5 flex-row items-center'
+                >
                     {navLists}
-                </div>
-            </header>
-            <hr />
-        </>
-    )
-}
+                </li>
+
+            </ul>
+
+            {/* Mobile Navigation Icon */}
+            <div onClick={handleNav} className='block mt-2 lg:hidden'>
+                {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+            </div>
+
+            {/* Mobile Navigation Menu */}
+            <ul
+                className={
+                    nav
+                        ? 'fixed lg:hidden left-0 top-0 w-[40%] h-full bg-gray-200  ease-in-out duration-500'
+                        : 'ease-in-out w-[40%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+                }
+            >
+                {/* Mobile Logo */}
+
+                <NavLink className="flex font-medium items-center mx-4  my-4 lg:my-0">
+                    <img src="https://i.ibb.co/7y8F1Gq/svg.png" alt="Miro" />
+                </NavLink>
+                {/* Mobile Navigation Items */}
+
+                <li
+
+                    className='mx-auto grid p-5 grid-cols-1 '
+                >
+                    {navLists}
+                </li>
+            </ul>
+        </div>
+    );
+};
 
 export default Navbar;
+
+
